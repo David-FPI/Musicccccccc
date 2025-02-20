4df9d1de8e17d351307c3b2f2ffb4c9d83e9b079
David-FPI <156213645+David-FPI@users.noreply.github.com>
2025-02-20 14:36:49 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 
-st.title("🎈 Ben
-")
+st.title("🎈 Ben ")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
